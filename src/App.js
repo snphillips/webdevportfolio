5168923dfb7c278b69363a8ace6300fc7abbdef4
Sarah Phillips <snphillips@gmail.com>
2018-10-02 10:39:29 -0400

    style for media queries

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ export default class App extends Component {
       <div>
         <div className="row align-items-center" id="header-statement">
           <div className="col-sm-12 col-md-5 col-lg-5 col-xl-5"><Header /></div>
-          <div className="col-sm-12 col-md-5 col-lg-5 col-xl-5"><Statement /></div>
-          <div className="col-sm-0 col-md-2 col-lg-2 col-xl-2"><Navigation /></div>
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6"><Statement /></div>
+          <div className="col-sm-0 col-md-1 col-lg-1 col-xl-1"><Navigation /></div>
         </div>
         <Projects />
         <About />
