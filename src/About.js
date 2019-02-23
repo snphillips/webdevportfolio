7fa88a92682bcdf0188d2f6aa7914a0e11286a30
Sarah Phillips <snphillips@gmail.com>
2019-02-23 10:11:01 -0500

    edit statement

--- src/About.js
+++ src/About.js
@@ -7,7 +7,12 @@ export default class About extends Component {
       <div className="contentblock" id="about">
         <hr />
         <h2 className="section-header">About</h2>
-        <p className="about-columns">I am a web developer and designer formally trained in art with a background in municipal service delivery. My experiences in community outreach and engagement have helped me learn to communicate with individuals from diverse backgrounds. I strive to create visually interesting solutions that are broadly appealing.  I believe tech should be intuitive, welcoming and delightful to interact with.</p>
+        <p className="about-columns">I am a web developer and designer formally trained in art
+        with a background in municipal service delivery. My experiences in community outreach
+        and engagement have helped me learn to communicate with individuals from diverse backgrounds.
+        I strive to create visually interesting solutions that are broadly appealing.  I believe tech
+        should be intuitive, welcoming and delightful to interact with. In my current position as a
+        Junior Interaction Designer at Triplelift, I create bespoke, consumer-centric ad templates.</p>
       </div>
     );
   }
