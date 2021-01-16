0af115dda9836e805f5942b818a205e958f54fa8
Sarah Phillips <snphillips@gmail.com>
2021-01-16 10:14:28 -0500

    edit about section

--- src/About.js
+++ src/About.js
@@ -8,12 +8,13 @@ export default class About extends Component {
         <hr />
         <h2 className="section-header">About</h2>
         <p className="about-columns">I am a software engineer and designer formally trained in art
-        with a background in municipal service delivery. My job working for the City of New York could
-        have taken place on the set of the show Parks & Recreation.  I strive to create visually interesting
-        solutions that are broadly appealing.  I believe tech should be intuitive, welcoming and delightful
-        to interact with. In my current position as an Interaction Engineer at Triplelift, I create bespoke,
+        with a background in municipal service delivery (My former position working for the City of New York could
+        have taken place on the set of the show Parks & Recreation).  In my current position as an Interaction Engineer at Triplelift, I create bespoke,
         consumer-centric ad templates and develop the in-house React application we used to create
         those templates.</p>
+
+        <p className="about-columns">I strive to create visually interesting solutions that are broadly appealing. I believe tech should be intuitive, welcoming and delightful
+        to interact with.</p>
       </div>
     );
   }
