18967f95bd77509045cc4da560bb710201edd6b3
Sarah Phillips <snphillips@gmail.com>
2021-12-10 15:16:36 -0500

    edit subheading

--- src/Statement.js
+++ src/Statement.js
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 
 const StatementStyled = styled.div`
   font-family: 'Crimson Text', serif;
-  font-size: 1.5em;
+  font-size: 2em;
   line-height: 1.2;
   font-weight: 400;
   font-style: italic;
-  margin-left: 3.5rem;
+  margin-left: 2.5rem;
   color: rgba(245, 208, 200, 0.8); /* dusty pink */
   z-index: -1;
 
@@ -22,38 +22,13 @@ const StatementStyled = styled.div`
     @media (max-width: 527px) {
       font-size: 1.2em;
     }
-  `
-
-// take StatementStyled and apply further customizations
-const StatementHighlight = styled.span`
-  cursor: pointer;
-  -webkit-transition: all 0.5s ease-in;
-  -moz-transition: all 0.5s ease-in;
-  -ms-transition: all 0.5s ease-in;
-  -o-transition: all 0.5s ease-in;
-  transition: all 0.5s ease-in;
-
-    &:hover {
-      cursor: pointer;
-      color: rgba(213, 11, 84, 0.9);
-      -webkit-transition: all 0.5s ease-out;
-      -moz-transition: all 0.5s ease-out;
-      -ms-transition: all 0.5s ease-out;
-      -o-transition: all 0.5s ease-out;
-      transition: all 0.5s ease-out;
-  }
-
-
-
 `;
 
 
 export default function Statement() {
     return (
 
-    <StatementStyled id="statement">
-      Software Developer: I create solutions that are <StatementHighlight>welcoming & delightful</StatementHighlight> to interact with.
-    </StatementStyled>
+    <StatementStyled id="statement">Software Engineer</StatementStyled>
 
     );
 
