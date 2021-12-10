c1e9559e5704008f1abcbf890dee55e0b7b2da4d
Sarah Phillips <snphillips@gmail.com>
2021-12-10 14:24:20 -0500

    edit About section

--- src/About.js
+++ src/About.js
@@ -13,10 +13,10 @@ export default class About extends Component {
           <AboutColumns>
             I am a Toronto-born, Brooklyn-based software developer formally trained in art
             with a background in municipal service delivery (My former position working for the City of New York could
-            have taken place on the set of the show Parks & Recreation).  In my current position as an Interaction Engineer at Triplelift, I create bespoke,
-            consumer-centric templates for programatic ads and develop the in-house React application we used to create
-            those templates. I strive to create visually interesting solutions that are broadly appealing. I believe tech should be intuitive, welcoming and delightful
-            to interact with.
+            have taken place on the set of the show Parks & Recreation).  Most recently, I worked for 2.5 years as an Interaction Engineer at TripleLift, 
+            where I created bespoke, consumer-centric templates for programatic ads and developed the in-house React application we used to create
+            those templates. I strive to create visually interesting solutions that are broadly appealing. 
+            I believe tech should be intuitive, welcoming and delightful to interact with.
           </AboutColumns>
       </ContentContainer>
     );
