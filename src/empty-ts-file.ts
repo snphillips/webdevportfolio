b65db03a51fa9cd89112102d627851fb38946dc9
Sarah Phillips <snphillips@gmail.com>
2023-07-11 19:58:18 -0400

    resolve errors

---
+++ src/empty-ts-file.ts
@@ -0,0 +1,7 @@
+// literally import anything, don't even have to use it
+import { Component} from 'react'; 
+
+
+it('test', () => {
+  console.log('this is a nothing component to resolve an error');
+});
