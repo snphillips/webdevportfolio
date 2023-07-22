1ec19bebbe4538604d3c0188a5b1422ae4fe0ee8
Sarah Phillips <snphillips@gmail.com>
2023-07-22 08:34:43 -0400

    add new resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,61 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Heading }  from "../user-interface-styles/headings.js"
-import { ContentContainer }  from "../user-interface-styles/layout.js"
-
+import { Heading } from '../user-interface-styles/headings.js';
+import { ContentContainer } from '../user-interface-styles/layout.js';
 
 const ContactUList = styled.ul`
   list-style: none;
   padding-left: 0;
-`
+`;
 
 const ContactListItemLink = styled.a`
-  color: rgba(255,255,255, 0.7); /* white */
-`
+  color: rgba(255, 255, 255, 0.7); /* white */
+`;
 
 // note: Heading & ContentContainer are "styled-components"
 
 export default function Contact(props) {
-
-    return (
-
-    <ContentContainer id="contact">
+  return (
+    <ContentContainer id='contact'>
       <hr />
       <Heading>Contact</Heading>
       <ContactUList>
         <li>snphillips [at] gmail [dot] com</li>
         <li>
           <ContactListItemLink
-            className="content-link"
-            href="https://www.linkedin.com/in/sarah-n-phillips/"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='content-link'
+            href='https://www.linkedin.com/in/sarah-n-phillips/'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-              LinkedIn
+            LinkedIn
           </ContactListItemLink>
         </li>
         <li>
           <ContactListItemLink
-            className="content-link"
-            href="https://github.com/snphillips"
-            target="_blank"
-            rel="noopener noreferrer">
-              Github
+            className='content-link'
+            href='https://github.com/snphillips'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Github
           </ContactListItemLink>
         </li>
         <li>
           <ContactListItemLink
-            className="content-link"
-            href="https://resume.creddle.io/resume/41xrld5p268"
-            target="_blank"
-            rel="noopener noreferrer">
-              View my resume here.
+            className='content-link'
+            href='https://sarahphillipsdev.surge.sh/2023-resume-phillips.pdf'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            View my resume here
           </ContactListItemLink>
         </li>
       </ContactUList>
-
     </ContentContainer>
-
-    );
-
+  );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,25 +4,25 @@ import styled from 'styled-components';
 const NavContainer = styled.div`
   display: block;
   position: fixed;
-`
+`;
 
 const NavItem = styled.a`
   position: relative;
   text-decoration: none; /* This removes the default underline */
-  color: rgba(255,255,255, 0.7);  /*This changes the default color*/
+  color: rgba(255, 255, 255, 0.7); /*This changes the default color*/
 
   &:hover {
     text-decoration: none; /* This removes the default underline */
-    color: rgba(255,255,255, 0.7); /* This changes the default color */
+    color: rgba(255, 255, 255, 0.7); /* This changes the default color */
   }
 
   &:before {
-    content: ""; /* If you put a string here, it will show up */
+    content: ''; /* If you put a string here, it will show up */
     position: absolute;
     left: 0; /* the position of the line */
     width: 100%; /* width of the line */
-    height: 2px;  /* thickness of the line */
-    bottom: -2px;   /*the space between the text and the line*/
+    height: 2px; /* thickness of the line */
+    bottom: -2px; /*the space between the text and the line*/
     background-color: rgba(213, 11, 84, 0.9); /*color:pink underline*/
     visibility: hidden; /* by default the underline is hidden*/
 
@@ -39,31 +39,43 @@ const NavItem = styled.a`
     -webkit-transform: scaleX(1); /* width of the line. 1 = 100% */
     transform: scaleX(1); /* width of the line. 1 = 100% */
   }
-
-
 `;
 
-
 export default function Navigation(props) {
+  return (
+    <NavContainer className='navbar navbar-expand-md navbar-dark mb-3'>
+      <button className='navbar-toggler' data-toggle='collapse' data-target='#navbarNav'>
+        <span className='navbar-toggler-icon'></span>
+      </button>
 
-  return(
-        <NavContainer className="navbar navbar-expand-md navbar-dark mb-3">
-          <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="ml-auto">
-              <li><NavItem href="#about">about</NavItem></li>
-              <li><NavItem href="#skills">skills</NavItem></li>
-              <li><NavItem href="#contact">contact</NavItem></li>
-              <li><NavItem href="https://www.linkedin.com/in/sarah-n-phillips/"><i className="fab fa-linkedin"></i></NavItem></li>
-              <li><NavItem href="https://github.com/snphillips"><i className="fab fa-github"></i></NavItem></li>
-            </ul>
-          </div>
-
-        </NavContainer>
-  )
-};
-
-
+      <div className='collapse navbar-collapse' id='navbarNav'>
+        <ul className='ml-auto'>
+          <li>
+            <NavItem href='#about'>about</NavItem>
+          </li>
+          <li>
+            <NavItem href='#skills'>skills</NavItem>
+          </li>
+          <li>
+            <NavItem href='#contact'>contact</NavItem>
+          </li>
+          <li>
+            <NavItem href='https://sarahphillipsdev.surge.sh/2023-resume-phillips.pdf'>
+              resume
+            </NavItem>
+          </li>
+          <li>
+            <NavItem href='https://www.linkedin.com/in/sarah-n-phillips/'>
+              <i className='fab fa-linkedin'></i>
+            </NavItem>
+          </li>
+          <li>
+            <NavItem href='https://github.com/snphillips'>
+              <i className='fab fa-github'></i>
+            </NavItem>
+          </li>
+        </ul>
+      </div>
+    </NavContainer>
+  );
+}
