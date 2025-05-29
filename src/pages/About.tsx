4a0fdb47393a11d5b32572e3096c1d16292c81ed
Sarah Phillips <snphillips@gmail.com>
2025-05-29 19:04:37 -0400

    li use sans-serif font

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,17 +10,22 @@ export default function About() {
       <hr />
       <Heading>About</Heading>
       <AboutColumns>
+
+        <p>
         I am a Brooklyn-based developer formally trained in art with a background in municipal
-        service delivery. <br />
-        <br />
+        service delivery.
+        </p>
+        <p>
         From 2022 to 2023 I was a Software Engineer for ed-tech startup Newsela where I worked on
         two React.js internal content management systems for the content creation team. Prior to
         that, I worked as an Interaction Engineer at the ad tech startup TripleLift where I created
         bespoke, consumer-centric templates for programatic ads and developed the in-house React
         application our team used to create those templates.
-        <br />
-        <br />I strive to create visually interesting solutions that are broadly appealing. I
+        </p>
+        <p>
+        I strive to create visually interesting solutions that are broadly appealing. I
         believe tech should be intuitive, welcoming and delightful to interact with.
+        </p>
       </AboutColumns>
     </ContentContainer>
   );
