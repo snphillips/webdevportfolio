bfeccde1fd646c68640488801e191235418f7204
Sarah Phillips <snphillips@gmail.com>
2025-05-29 13:14:45 -0400

    upgrade packages including React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import './App.css';
 import Header from './components/Header';
