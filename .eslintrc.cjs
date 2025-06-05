b6be706c9f1d2b555964e2650757f714b80b2928
Sarah Phillips <snphillips@gmail.com>
2025-06-05 17:54:10 -0400

    configure prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,116 +1,119 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ["@typescript-eslint", "react", "import"],
+  plugins: ['@typescript-eslint', 'react', 'import'],
   env: {
     browser: true,
     node: true,
     es2021: true,
   },
   extends: [
-    "eslint:recommended",                      // ESLint’s built‐in recommended rules
-    "plugin:@typescript-eslint/recommended",   // recommended for TS
-    "plugin:react/recommended",                // recommended for React
+    'eslint:recommended', // ESLint’s built‐in recommended rules
+    'plugin:@typescript-eslint/recommended', // recommended for TS
+    'plugin:react/recommended', // recommended for React
     // …any other shareable configs you use
   ],
   settings: {
     react: {
-      version: "detect",                       // for eslint-plugin-react
+      version: 'detect', // for eslint-plugin-react
     },
   },
   rules: {
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-indent': ['error', 2],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: { var: 2, let: 2, const: 3 },
+        outerIIFEBody: 1,
+        MemberExpression: 1,
+        FunctionDeclaration: { parameters: 1, body: 1 },
+        FunctionExpression: { parameters: 1, body: 1 },
+        CallExpression: { arguments: 1 },
+        ArrayExpression: 1,
+        ObjectExpression: 1,
+        ImportDeclaration: 1,
+        flatTernaryExpressions: false,
+        ignoredNodes: [
+          'JSXElement',
+          'JSXElement *',
+          'JSXFragment',
+          'JSXFragment *',
+        ],
+        ignoreComments: false,
+      },
+    ],
 
-      "react/jsx-indent-props": ["error", 2],
-  "react/jsx-indent": ["error", 2],
-   
-    "quotes": ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
+    semi: ['error', 'always'],
 
-   
-    "indent": ["error", 2, {
-      "SwitchCase": 1,
-      "VariableDeclarator": { "var": 2, "let": 2, "const": 3 },
-      "outerIIFEBody": 1,
-      "MemberExpression": 1,
-      "FunctionDeclaration": { "parameters": 1, "body": 1 },
-      "FunctionExpression": { "parameters": 1, "body": 1 },
-      "CallExpression": { "arguments": 1 },
-      "ArrayExpression": 1,
-      "ObjectExpression": 1,
-      "ImportDeclaration": 1,
-      "flatTernaryExpressions": false,
-      "ignoredNodes": ["JSXElement", "JSXElement *", "JSXFragment", "JSXFragment *"],
-      "ignoreComments": false
-    }],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
 
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreStrings: true,
+        ignoreUrls: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
 
-    "semi": ["error", "always"],
+    'no-trailing-spaces': 'error',
 
- 
-    "comma-dangle": ["error", {
-      "arrays":    "always-multiline",
-      "objects":   "always-multiline",
-      "imports":   "always-multiline",
-      "exports":   "always-multiline",
-      "functions": "never"
-    }],
+    'eol-last': ['error', 'always'],
 
+    'keyword-spacing': [
+      'error',
+      {
+        before: true,
+        after: true,
+        overrides: {
+          return: { after: true },
+          throw: { after: true },
+          try: { after: true },
+        },
+      },
+    ],
 
-    "max-len": ["error", {
-      "code": 100,
-      "tabWidth": 2,
-      "ignoreComments": true,
-      "ignoreTrailingComments": true,
-      "ignoreStrings": true,
-      "ignoreUrls": true,
-      "ignoreTemplateLiterals": true,
-      "ignoreRegExpLiterals": true
-    }],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
 
+    'import/newline-after-import': ['error', { count: 1 }],
 
-    "no-trailing-spaces": "error",
+    'react/jsx-curly-spacing': ['error', { when: 'always', children: true }],
 
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
 
-    "eol-last": ["error", "always"],
+    'linebreak-style': ['error', 'unix'],
 
-    "keyword-spacing": ["error", {
-      "before": true,
-      "after": true,
-      "overrides": {
-        "return": { "after": true },
-        "throw":  { "after": true },
-        "try":    { "after": true }
-      }
-    }],
-
-
-    "object-curly-spacing": ["error", "always"],
-    "array-bracket-spacing": ["error", "never"],
-
-
-    "import/newline-after-import": ["error", { "count": 1 }],
-
- 
-    "jsx-quotes": ["error", "prefer-single"],
-
-
-    "react/jsx-curly-spacing": ["error", { "when": "always", "children": true }],
-
-
-    "@typescript-eslint/no-unused-vars": ["error", {
-      "argsIgnorePattern": "^_",
-      "varsIgnorePattern": "^_"
-    }],
-
-
-    "linebreak-style": ["error", "unix"],
-
-    "react/react-in-jsx-scope": "off"
-  }
+    'react/react-in-jsx-scope': 'off',
+  },
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
-
 export default function Header() {
   return (
     <header className="site-header">
-                <h1>Sarah Phillips</h1>
+      <h1>Sarah Phillips</h1>
       <div className="tagline-container">
         <p className="tagline">Software Engineer</p>
       </div>
